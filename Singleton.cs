ce146cce2116a643c58dc0392a2f9c1bdc7a1837
Comp-3 Interactive <Comp3interactive@users.noreply.github.com>
2020-01-05 18:18:45 +0000

    Initial commit
    Initial commit

---
+++ Singleton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+            instance = (T)FindObjectOfType(typeof(T));
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ SingletonDontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SingletonDontDestroy<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = (T)FindObjectOfType(typeof(T));
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
